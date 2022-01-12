793119d24e3f2cc223d6279acb4814eb9244b701
ATCLeHue <hue.le@at-sky.com.vn>
2022-01-12 17:06:27 +0700

    add query vcb

---
+++ stocks.js
@@ -0,0 +1,18 @@
+class Stocks{
+    constructor(el){
+        this.doc = el;
+        this.getPrice("VCB");
+    }
+}
+Stocks.prototype.getPrice = function(stock){
+    var _this = this;
+    fetch('https://api.khochangchang.com/api/stocks')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        var td = _this.doc.createElement("tr");
+        td.innerHTML = `<td>${ data.name }</td><td><div class="${ data.vector }">${ data.price }</div></td>`
+        _this.doc.getElementById("stocks").getElementsByTagName("tbody")[0].append(td);
+    });
+}
+new Stocks(document);

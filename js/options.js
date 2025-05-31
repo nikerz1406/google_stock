4cbf74ac78e49c1e9e9fbe8741cc83f44c5a0792
nikerz1406 <hue.le@slimcase.co>
2025-05-31 10:49:05 +0700

    refactor code

--- js/options.js
+++ js/options.js
@@ -2,173 +2,218 @@ import Stocks from './stocks.js';
 
 chrome.storage.sync.get("stocks", ({ stocks }) => {
     Options.renderStocks(stocks);
-  });
+});
 
-class Options{
-    constructor(){
-        this.addEventListener()
+/**
+ * Options class handles UI and storage for stock options.
+ */
+class Options {
+    constructor() {
+        this.addEventListener();
     }
-    static renderStocks = function(stocks){
-        var tbody = document.getElementById("stocks").getElementsByTagName("tbody")[0];
-        var html = '';
-        
-        if(stocks){
-            var key = 1;
-            stocks.forEach(function(s){
-                html = Options.row({key,name:s.name,server:s.server});
+
+    /**
+     * Render stocks into the table body.
+     * @param {Array} stocks
+     */
+    static renderStocks = (stocks) => {
+        const tbody = document.getElementById("stocks").getElementsByTagName("tbody")[0];
+        let html = '';
+        tbody.innerHTML = '';
+        if (stocks && stocks.length) {
+            let key = 1;
+            for (const s of stocks) {
+                html = Options.row({ key, name: s.name, server: s.server });
                 key++;
                 tbody.appendChild(html);
-            })
+            }
             return;
         }
-        
         // default empty row
-        html = Options.row({key:1});    
+        html = Options.row({ key: 1 });
         tbody.appendChild(html);
-        
     }
-    static row = function(data){
-        
-        var key = data.key ? data.key : 1;
-        var name = data.name ? data.name : '';
-        var server = data.server ? data.server : 1;
-        var tr = document.createElement("tr");
+
+    /**
+     * Create a table row for a stock.
+     * @param {Object} data
+     * @returns {HTMLTableRowElement}
+     */
+    static row = ({ key = 1, name = '', server = 1 }) => {
+        const tr = document.createElement("tr");
         tr.dataset.index = key;
-        tr.innerHTML = `<td class="align-middle ps-3">${ key }</td>
+        tr.innerHTML = `<td class="align-middle ps-3">${key}</td>
         <td class="align-middle"><input type="text" class="text-uppercase stock-name" name="stock[${key}][name]" value="${name}"></td>
-        <td class="align-middle text-center">` + Options.bootstrapRadio({key,value:1,server}) + `</td>
-        <td class="align-middle text-center">` + Options.bootstrapRadio({key,value:2,server}) + `</td>
-        <td class="align-middle text-center">` + Options.bootstrapRadio({key,value:3,server}) + `</td>
+        <td class="align-middle text-center">${Options.bootstrapRadio({ key, value: 1, server })}</td>
+        <td class="align-middle text-center">${Options.bootstrapRadio({ key, value: 2, server })}</td>
+        <td class="align-middle text-center">${Options.bootstrapRadio({ key, value: 3, server })}</td>
         <td><button class="float-end btn btn-sm btn-outline-danger px-3" data-btn="remove${key}" title="remove row"><i class="fa fa-trash me-1" aria-hidden="true"></i> Remove</button></td>`;
         return tr;
-    
     }
-    static bootstrapRadio = function(data){
-        var key = data.key ? data.key : 1;
-        var value = data.value ? data.value : 1;
-        var isCheck = value == data.server ? 'checked' : '';
-        
+
+    /**
+     * Create a radio input for server selection.
+     * @param {Object} data
+     * @returns {string}
+     */
+    static bootstrapRadio = ({ key = 1, value = 1, server = 1 }) => {
+        const isCheck = value == server ? 'checked' : '';
         return `<div class="justify-content-center align-items-center">
             <input class="form-check-input" type="radio" name="stock[${key}][server]" value="${value}" ${isCheck}>
-        </div>`
+        </div>`;
     }
-    static bootstrapPrice = function(data){
-        var price = data.price ? data.price : '-';
-        var color = data.vector ? ( data.vector == 'up' ? 'success' : (data.vector == 'down' ? 'danger' : 'warning')) : 'danger';
-        var icon = data.vector ? ( data.vector == 'up' ? 'fa fa-sort-asc' : (data.vector == 'down' ? 'fa fa-caret-down' : 'fa fa-minus')) : 'fa fa-caret-down';
+
+    /**
+     * Create a price display with color and icon.
+     * @param {Object} data
+     * @returns {string}
+     */
+    static bootstrapPrice = ({ price = '-', vector = 'down' }) => {
+        const color = vector === 'up' ? 'success' : (vector === 'down' ? 'danger' : 'warning');
+        const icon = vector === 'up' ? 'fa fa-sort-asc' : (vector === 'down' ? 'fa fa-caret-down' : 'fa fa-minus');
         return `<span class="text-${color}">${price}  <i class="ms-1 align-middle ${icon} text-${color}" aria-hidden="true"></i></span>`;
     }
-    static rowServerAlive = function(data){
-        var key = data.key ? data.key : 1;
-        var name = data.name ? data.name : '';
-        var tr = document.createElement("tr");
-        var server = data.server ? data.server : [];
+
+    /**
+     * Create a row for server alive status.
+     * @param {Object} data
+     * @returns {HTMLTableRowElement}
+     */
+    static rowServerAlive = ({ key = 1, name = '', server = [] }) => {
+        const tr = document.createElement("tr");
         tr.dataset.index = key;
-        tr.innerHTML = `<td class="align-middle ps-3 ">${ key }</td>
+        tr.innerHTML = `<td class="align-middle ps-3 ">${key}</td>
         <td class="align-middle text-uppercase">${name}</td>
-        <td class="align-middle text-center">`+Options.bootstrapPrice(server[0])+`</td>
-        <td class="align-middle text-center">`+Options.bootstrapPrice(server[1])+`</td>
-        <td class="align-middle text-center">`+Options.bootstrapPrice(server[2])+`</td>`;
+        <td class="align-middle text-center">${Options.bootstrapPrice(server[0] || {})}</td>
+        <td class="align-middle text-center">${Options.bootstrapPrice(server[1] || {})}</td>
+        <td class="align-middle text-center">${Options.bootstrapPrice(server[2] || {})}</td>`;
         return tr;
     }
-}
 
-Options.prototype.addEventListener = function(){
-    var _this = this;
-    document.getElementById("add").addEventListener("click",function(){
-        _this.add();
-        _this.save();
-    })
-
-    document.getElementById("stocks").getElementsByTagName("tbody")[0].addEventListener('click',function(e){
-        if(e.target.tagName == 'BUTTON'){
-            //do something
-            _this.remove(e.target);
-            _this.save();
-        }
-    })
-
-    document.getElementById("refresh-query").addEventListener('click',function(){
-        _this.refresh();
-    })
-}
-Options.prototype.getCurrentStocks = function(){
-    var body = document.getElementById("stocks").getElementsByTagName("tbody")[0];
-    var nodes = body.querySelectorAll("tr");
-    var stocks = [];
-    nodes.forEach(function(e){
-        var txt = e.querySelectorAll("td")[1].getElementsByTagName("input")[0].value;
-        // var server = e.querySelector("input[type='radio']:checked").value;
-        var radios = e.getElementsByClassName("form-check-input")
-        var server = 1;
-        Array.from(radios, (element, index) => { 
-            if(element.checked){
-                server = element.value;
+    /**
+     * Add all event listeners for the options UI.
+     */
+    addEventListener = () => {
+        document.getElementById("add").addEventListener("click", () => {
+            this.add();
+            this.save();
+        });
+        document.getElementById("stocks").getElementsByTagName("tbody")[0].addEventListener('click', (e) => {
+            if (e.target.tagName === 'BUTTON') {
+                this.remove(e.target);
+                this.save();
             }
-        })
-
-        if(txt!='') stocks.push({name:txt,server});
-    })
-    return stocks;
-}
-Options.prototype.save = function(){
-    var stocks = this.getCurrentStocks();
-    console.log({stocks})
-    chrome.storage.sync.set({ stocks });
-}
-Options.prototype.add = function(){
-    var body = document.getElementById("stocks").getElementsByTagName("tbody")[0];
-    var nodes = body.querySelectorAll("tr");
-    var key = 1;
-    if(nodes.length != 0){
-        var last_node = nodes[nodes.length -1];
-        key = last_node.dataset.index;
-        key = key ? parseInt(key)+1 : 1;
+        });
+        document.getElementById("refresh-query").addEventListener('click', () => {
+            this.refresh();
+        });
     }
-    
-    var html = Options.row({key});
-    body.appendChild(html);
-    this.addTriggerFocusSave(html);
-    
+
+    /**
+     * Get current stocks from the table.
+     * @returns {Array}
+     */
+    getCurrentStocks = () => {
+        const body = document.getElementById("stocks").getElementsByTagName("tbody")[0];
+        const nodes = body.querySelectorAll("tr");
+        const stocks = [];
+        nodes.forEach(e => {
+            const txt = e.querySelectorAll("td")[1].getElementsByTagName("input")[0].value;
+            const radios = e.getElementsByClassName("form-check-input");
+            let server = 1;
+            Array.from(radios).forEach(element => {
+                if (element.checked) {
+                    server = element.value;
+                }
+            });
+            if (txt !== '') stocks.push({ name: txt, server });
+        });
+        return stocks;
+    }
+
+    /**
+     * Save current stocks to chrome storage.
+     */
+    save = () => {
+        const stocks = this.getCurrentStocks();
+        console.log({ stocks });
+        chrome.storage.sync.set({ stocks });
+    }
+
+    /**
+     * Add a new stock row.
+     */
+    add = () => {
+        const body = document.getElementById("stocks").getElementsByTagName("tbody")[0];
+        const nodes = body.querySelectorAll("tr");
+        let key = 1;
+        if (nodes.length !== 0) {
+            const last_node = nodes[nodes.length - 1];
+            key = last_node.dataset.index;
+            key = key ? parseInt(key) + 1 : 1;
+        }
+        const html = Options.row({ key });
+        body.appendChild(html);
+        this.addTriggerFocusSave(html);
+    }
+
+    /**
+     * Add focusout event to save and refresh when input loses focus.
+     * @param {HTMLElement} element
+     */
+    addTriggerFocusSave = (element) => {
+        element.addEventListener('focusout', () => {
+            this.save();
+            this.refresh();
+        });
+    }
+
+    /**
+     * Refresh the table numbers and price server alive.
+     */
+    refresh = () => {
+        this.refreshNo();
+        this.refreshPrice();
+    }
+
+    /**
+     * Refresh price server alive for all stocks.
+     */
+    refreshPrice = async () => {
+        const stocks = this.getCurrentStocks();
+        let key = 1;
+        const tbody = document.getElementById("stocks-realtime").getElementsByTagName("tbody")[0];
+        tbody.innerHTML = '';
+        for (const stock of stocks) {
+            const data = await Stocks.getPriceWithServer(stock.name, "all");
+            data.key = key;
+            const html = Options.rowServerAlive(data);
+            tbody.append(html);
+            key++;
+        }
+    }
+
+    /**
+     * Refresh row numbers in the table.
+     */
+    refreshNo = () => {
+        let no = 1;
+        document.getElementById("stocks").getElementsByTagName("tbody")[0].querySelectorAll("tr").forEach(e => {
+            e.getElementsByTagName("td")[0].innerHTML = no;
+            no++;
+        });
+    }
+
+    /**
+     * Remove a stock row.
+     * @param {HTMLElement} el
+     */
+    remove = (el) => {
+        el.closest("tr").remove();
+        this.refresh();
+    }
 }
-Options.prototype.addTriggerFocusSave = function(element){
-    var _this = this;
-    element.addEventListener('focusout',function(){
-        _this.save();
-        _this.refresh();
-    })
-    
-}
-Options.prototype.refresh = function (){
-    // reset number table
-    this.refreshNo();
-    // reset price server alive
-    this.refreshPrice();
-}
-Options.prototype.refreshPrice = function(){
-    var stocks = this.getCurrentStocks();
-    var key = 1;
-    var tbody = document.getElementById("stocks-realtime").getElementsByTagName("tbody")[0];
-    tbody.innerHTML = '';
-    stocks.forEach(async stock => {
-        var data = await Stocks.getPriceWithServer(stock.name,"all");
-        data.key = key;
-        var html = Options.rowServerAlive(data);
-        tbody.append(html);
-        key++;
-    });
-}
-Options.prototype.refreshNo = function(){
-    let no = 1;
-    document.getElementById("stocks").getElementsByTagName("tbody")[0].querySelectorAll("tr").forEach(e=>{
-        e.getElementsByTagName("td")[0].innerHTML = no;
-        no++;
-    })
-}
-Options.prototype.remove = function(el){
-    el.closest("tr").remove();
-    this.refresh();
-}
+
 document.addEventListener("DOMContentLoaded", () => {
     new Options(document);
 });
--- js/stocks.js
+++ js/stocks.js
@@ -1,38 +1,64 @@
 import endpoints from '../config/api.js'
-export default class Stocks{
-    constructor(el){
-        
+/**
+ * Stocks class handles fetching and rendering stock data.
+ */
+export default class Stocks {
+    /**
+     * @param {HTMLElement} el - The element to attach stock data to (currently unused)
+     */
+    constructor(el) {
+        // Reserved for future use
     }
-    static row = function(data){
-        var name = data.stock ? data.stock.toUpperCase() : '';
-        var vector = data.vector ? data.vector : 'eq';
-        var price = data.price ? data.price : 0;
-        return `<td>${ name }</td><td><div class="${ vector }">${ price }</div></td>`;
-    }
-    static renderStocks =  function(stocks){
-        stocks.forEach(async stock => {
-            var data = await Stocks.getPrice(stock);
-            var td = document.createElement("tr");
-            td.innerHTML = Stocks.row(data);
-            document.getElementById("stocks").getElementsByTagName("tbody")[0].append(td);
-        });
-    }
-    static getPrice = async function(stock){
-        var url = endpoints + stock.name + "/" + stock.server ;
-        return await fetch(url)
-        .then(response => response.json())
-        .then(res => {
-            return res.data;
-        });
-    }
-    static getPriceWithServer = async function (stock,server = '') {
 
-        // enum server : [1,2,3,all,'']
-        var url = endpoints + stock +'/'+server;
-        return await fetch(url)
-        .then(response => response.json())
-        .then(res => {
-            return res.data.stock;
-        });
+    /**
+     * Generate a table row for a stock.
+     * @param {Object} data - Stock data
+     * @param {string} data.stock - Stock name
+     * @param {string} data.vector - Price direction (e.g., 'up', 'down', 'eq')
+     * @param {number} data.price - Stock price
+     * @returns {string} HTML string for the table row
+     */
+    static row = ({ stock = '', vector = 'eq', price = 0 }) => {
+        const name = stock.toUpperCase();
+        return `<td>${name}</td><td><div class="${vector}">${price}</div></td>`;
+    }
+
+    /**
+     * Render multiple stocks into the table body with id 'stocks'.
+     * @param {Array} stocks - Array of stock objects
+     */
+    static renderStocks = async (stocks) => {
+        const tbody = document.getElementById('stocks').getElementsByTagName('tbody')[0];
+        for (const stock of stocks) {
+            const data = await Stocks.getPrice(stock);
+            const tr = document.createElement('tr');
+            tr.innerHTML = Stocks.row(data);
+            tbody.append(tr);
+        }
+    }
+
+    /**
+     * Fetch price data for a stock.
+     * @param {Object} stock - Stock object with 'name' and 'server'
+     * @returns {Promise<Object>} Stock data
+     */
+    static getPrice = async (stock) => {
+        const url = endpoints + stock.name + '/' + stock.server;
+        const response = await fetch(url);
+        const res = await response.json();
+        return res.data;
+    }
+
+    /**
+     * Fetch price data for a stock with a specific server.
+     * @param {string} stock - Stock name
+     * @param {string|number} [server=''] - Server identifier
+     * @returns {Promise<Object>} Stock data
+     */
+    static getPriceWithServer = async (stock, server = '') => {
+        const url = endpoints + stock + '/' + server;
+        const response = await fetch(url);
+        const res = await response.json();
+        return res.data.stock;
     }
 }
